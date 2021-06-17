a0272ce6d919eddf814a0e49530952ee1010fb1d
Stefano <piero.quiroz93@gmail.com>
2021-06-17 17:00:44 -0500

    Criando os componentes react da app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
