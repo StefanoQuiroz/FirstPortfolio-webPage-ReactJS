8b0990a93389fbadda7af111bccf582b136cf536
Stefano <piero.quiroz93@gmail.com>
2021-06-19 18:28:45 -0500

    Contact Finished

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import PageHeader from './PageHeader';
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 const Contact = () => {
+    const handleUrl = (url) => (()=> window.open(url, "_blank"))
     return (
         <div className="contacts">
-            <PageHeader title={"Contacts"}/>
+            <PageHeader title={"Contact-Me"}/>
+            <div className="contactsIcons">
+                <FaGithub 
+                color="white" 
+                size="3.5rem" 
+                style={{padding:"1%"}}
+                onClick={handleUrl("https://github.com/StefanoQuiroz")}
+                />
+                <FaLinkedin 
+                color="white" 
+                size="3.5rem" 
+                style={{padding:"1%"}}
+                onClick={handleUrl("https://www.linkedin.com/in/stefano-quiroz-597403210/")}
+                />
+            </div>    
         </div>
     );
 }
