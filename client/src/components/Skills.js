ef6011cc5cde7e085635b37f28c92e328f3bca94
Stefano <piero.quiroz93@gmail.com>
2021-06-18 20:33:57 -0500

    Skills falta a lista das ferramentas back-front-server

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import PageHeader from './PageHeader';
+
+
 
 const Skills = () => {
     return (
-        <div>
-            <h2>Skills</h2>
+        <div className="skills">
+            <PageHeader title={"My skills"}/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum explicabo architecto nulla. Nihil eligendi consequuntur quia, suscipit accusamus quaerat cumque non perspiciatis aut, adipisci officia dolore, at ab! Voluptas molestias impedit pariatur voluptatem doloribus illum aliquid nihil eveniet molestiae quis temporibus mollitia libero non et, ducimus iste inventore praesentium amet, nesciunt esse officiis. Quis commodi exercitationem placeat pariatur sequi ipsum dignissimos hic esse molestiae voluptate.</p>
         </div>
     );
 }
