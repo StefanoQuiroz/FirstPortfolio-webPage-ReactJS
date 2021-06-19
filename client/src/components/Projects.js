01746654b0df72a40c7d1ae558aa528b278b312f
Stefano <piero.quiroz93@gmail.com>
2021-06-19 17:43:48 -0500

    Projets finished

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,24 +2,37 @@ import React from 'react';
 import PageHeader from './PageHeader';
 import github1 from '../images/github1.gif';
 import github2 from '../images/github2.gif';
+import covery from '../images/cover.jpg';
+import logoGit from '../images/github.svg'
 
 const Projects = () => {
     const descriptionGitHub1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur fuga provident error adipisci doloremque quos nihil corporis. Reiciendis, dolores facilis vitae maxime enim sequi nulla?"
 
     const descriptionGitHub2 = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis eius totam enim fugit dolores, maiores, numquam quod corrupti voluptas aperiam vitae illum eveniet autem quis!"
-
+    
+    const pHandler = (url) => (
+        <div className="logoGit" onClick={e => window.open(url, "_blank").focus}
+        >           
+            <img src={logoGit} alt="logoGit" />
+        </div>
+    )
     const projectList = (url, img, description) => (
         <div className="projectListWrapper">
             <div className="github">
                 <img src={img} alt="img" />
             </div>
             <div className="projectListText">
-                <p>{description}</p>
+                <p>
+                    {description}
+                    {pHandler(url)}
+                </p>
+                
             </div>
         </div>
     );
     return (
         <div className="projects">
+            <img className="covery" src={covery} alt="covery" />
             <PageHeader title={"My Projects available"}/>
             <div className="wrapperProjects">
                 <div className="projectText">
